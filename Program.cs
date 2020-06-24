c9079fb8b32c24282f38b243958b34eed5af0a93
altryx <dlampa@gmail.com>
2020-06-24 15:39:55 -0600

    Add retrieve user input; add string interpolation.

--- Program.cs
+++ Program.cs
@@ -10,13 +10,24 @@ namespace c_sharp_intro
             // We are assigning a string value to a variable.
             string myName = "Damir Lampa";
             /* We begin multi-line comments with a forward-slash and asterisk...
-
+ 
             Symbols between values or evaluations are considered to be "operators"
             Below we are using the addition symbol to concatenate two strings.
             (Concatenation means gluing two strings, or texts, together.)
 
             and we end multi-line comments with an asterisk followed by a forward-slash */
             Console.WriteLine("Hello, " + myName);
+            // User input...
+            Console.WriteLine("Please enter your name...");
+            // Assign variable 'userName' to a value entered by the user.
+            string userName = Console.ReadLine();
+            // Show the user we collected the name.
+            Console.WriteLine("You have entered: " +  userName);
+            // Instead of traditional concatenation using the addition operator...
+            // We can use string interpolation (placeholders) to embed values in
+            // a string!
+            Console.WriteLine("Hey {0}, you're looking great today!", userName);
+
         }
     }
 }
