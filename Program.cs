605975c8df8b671c3efd9b53cbcfbbb4f1de70bf
altryx <dlampa@gmail.com>
2020-06-25 13:39:56 -0600

    Add addition calculation (collect 2 numbers and output sum)

--- Program.cs
+++ Program.cs
@@ -6,46 +6,20 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            // Logical operators.
-            // OR will evaluate to TRUE if EITHER side is TRUE.
-            Console.WriteLine(true || true); // || is OR. Returns true
-            Console.WriteLine(false || true); // true
-            Console.WriteLine(false || false); // false
-
-            // AND will evaluate to TRUE only if BOTH sides are TRUE.A
-            Console.WriteLine(true && true); // && is AND
-            Console.WriteLine(false && true); // false
-            Console.WriteLine(false && false); // false
-
-            // How does this behave with different data-types?
-            // console.writeline(true && 1); //error - mixing bool and int
-
-            // Comparative operators...
-            // "Equal to" check
-            Console.WriteLine(false == false); // true
-            Console.WriteLine(true == false); // false
-            Console.WriteLine(2.25 == 2.250); // true
-            // Console.WriteLine(2.25 == "2.25"); // can't be compared - different types
-
-            // "Greater than" check
-            Console.WriteLine(6 > 7); // false
-            Console.WriteLine(10 > 5); // true
-
-            // "Less than" check
-            Console.WriteLine(5 < 4); //false
-            Console.WriteLine(6 < 7); //true
-
-            // "Greater than or equal to" check
-            Console.WriteLine(5 >= 5); // true
-
-            // "Less than or equal to" check
-            Console.WriteLine(3 <= 4); // true
-
-            // "NOT equal to" operator
-            Console.WriteLine(false != false); // false
-            Console.WriteLine(true != false); // true
-
+            Console.WriteLine("Please enter the operation (add/subtract): ");
+            string userOperator = Console.ReadLine();
             
+            if (userOperator == "add")
+            {
+                Console.WriteLine("Please enter the first number: ");
+                int firstNum = Int32.Parse(Console.ReadLine()); // convert to int32/integer
+                Console.WriteLine("Please enter the second number: ");
+                int secondNum = Int32.Parse(Console.ReadLine()); // convert to int32/integer
+                int result = firstNum + secondNum;
+                Console.WriteLine("{0} + {1} = {2}", firstNum, secondNum, result);
+
+            } 
+
 
         }
     }
